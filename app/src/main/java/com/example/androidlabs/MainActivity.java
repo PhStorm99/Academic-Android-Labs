adf4b8cdd8ebbcbd74cfae36640a60b02503ac04
PhStorm99 <hnpavalonn504@gmail.com>
2019-10-02 19:00:14 -0400

    Finished Lab 3

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,19 +1,88 @@
 package com.example.androidlabs;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.CheckBox;
-
 public class MainActivity extends AppCompatActivity {
+ //   protected Button mybutton;
+
+    SharedPreferences sp;
+    EditText typeField;
+    public static final String ACTIVITY_NAME = "MainACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main);
+
+        typeField =(EditText)findViewById(R.id.editText1);
+        sp = getSharedPreferences("UserEmail", Context.MODE_PRIVATE);
+
+        String savedEmail = sp.getString("EmailEntered","");
+        typeField.setText(savedEmail);
+
+        Button loginButton = (Button)findViewById(R.id.button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View c) {
+
+                Intent profilePage = new Intent(MainActivity.this, ProfileActivity.class);
+                //Give directions to go from this page, to ProfileActivity page
+
+                profilePage.putExtra("EmailFormLastPage", typeField.getText().toString());
+
+                //Now make the transition:
+                MainActivity.this.startActivity(profilePage);
+            }
+        });
+
     }
 
 
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        //object editor
+        SharedPreferences.Editor editor = sp.edit();
+
+        //saving Email address on EmailEntered
+        String email = typeField.getText().toString();
+        editor.putString("EmailEntered",email);
+
+        //to save the changes
+        editor.commit();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(ACTIVITY_NAME ,"In function:  onStop");    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(ACTIVITY_NAME ,"In function:  onDestroy");    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(ACTIVITY_NAME ,"In function:  onStart");    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(ACTIVITY_NAME ,"In function:  onResume");
+    }
 
 
 }
---
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -0,0 +1,92 @@
+package com.example.androidlabs;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity{
+
+    EditText enterText;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageButton mImageButton;
+    public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.e(ACTIVITY_NAME ,"In function:  onCreate");
+        setContentView(R.layout.profile_activity);
+
+        enterText = (EditText)findViewById(R.id.editText4);
+
+        // get the intent that got us here
+        Intent lastPage = getIntent();
+        String previousTypedEmail = lastPage.getStringExtra("EmailFormLastPage");
+        //Put the string that was sent from FirstActivity into the edit text
+        enterText.setText(previousTypedEmail);
+
+
+        mImageButton = (ImageButton)findViewById(R.id.picture);
+        mImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View c) {
+                ProfileActivity.this.dispatchTakePictureIntent();
+            }
+        });
+
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.e(ACTIVITY_NAME,"In function: onActivityResult");
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageButton.setImageBitmap(imageBitmap);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(ACTIVITY_NAME ,"In function:  onStop");    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(ACTIVITY_NAME ,"In function:  onDestroy");    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(ACTIVITY_NAME ,"In function:  onStart");    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(ACTIVITY_NAME ,"In function:  onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(ACTIVITY_NAME ,"In function:  onResume");
+    }
+
+}
